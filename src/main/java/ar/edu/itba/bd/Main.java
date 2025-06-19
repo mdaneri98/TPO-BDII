3216cfe75113f21584b1d39068bf8e0547abb6e3
dlimachi <dlimachi@itba.edu.ar>
2025-06-18 23:39:34 -0300

    modified mongo connection

--- src/main/java/ar/edu/itba/bd/Main.java
+++ src/main/java/ar/edu/itba/bd/Main.java
@@ -1,9 +1,13 @@
 package ar.edu.itba.bd;
 
-import ar.edu.itba.bd.controllers.PingController;
+import ar.edu.itba.bd.database.MongoConnection;
 import ar.edu.itba.bd.database.MongoService;
+import ar.edu.itba.bd.dto.Proveedor;
 import io.javalin.Javalin;
-import static io.javalin.apibuilder.ApiBuilder.*;
+import com.mongodb.client.*;
+import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -12,10 +16,31 @@ public class Main {
         MongoService mongoService = new MongoService();
 
         Javalin app = Javalin.create(config -> {
-            config.router.apiBuilder(() -> {
-                get("/ping", PingController::ping);
-            });
-        }).start(7070);
+            config.defaultContentType = "application/json";
+        });
 
+        app.get("/ping", ctx -> {
+            ctx.result("pong");
+        });
+
+        app.get("/proveedores", ctx -> {
+            MongoDatabase db = MongoConnection.getDatabase("tp2025");
+            MongoCollection<Document> col = db.getCollection("proveedor");
+            FindIterable<Document> docs = col.find();
+
+            List<Proveedor> proveedores = new ArrayList<>();
+            for (Document doc : docs) {
+                Proveedor p = new Proveedor();
+                p.cuit = doc.getString("cuit");
+                p.razon_social = doc.getString("razon_social");
+                p.activo = doc.getBoolean("activo", false);
+                p.habilitado = doc.getBoolean("habilitado", false);
+                p.telefonos = (List<String>) doc.get("telefonos");
+                proveedores.add(p);
+            }
+            ctx.json(proveedores); 
+        });
+
+        app.start(7070);
     }
 }
---
+++ src/main/java/ar/edu/itba/bd/dto/Proveedor.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.bd.dto;
+
+import java.util.List;
+
+public class Proveedor {
+    public String cuit;
+    public String razon_social;
+    public boolean activo;
+    public boolean habilitado;
+    public List<String> telefonos;
+}
